afcfc83dc6e1009b7fee31ff38e068bb3fa301f8
simpleC0de <hetznecker2@gmail.com>
2021-12-22 15:50:26 +0100

    Update main.py
    Added current time before console logs and the log file
    Using input as a way to get the user data (no hardcoded credentials required anymore)
    Reduced checktime to each minute instead of 10 minutes (changeable on line 71)

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from os import sep
 from os import path
 from instagrapi import Client # https://github.com/adw0rd/instagrapi
 import time
+from datetime import datetime
 
-user_to_observe = "USER TO TRACK"
-username = "UR USER"
-password = "UR PASSWORD"
+user_to_observe = input("Username to track: ")
+username = input("Your account login: ")
+password = input("Your account password: ")
 
 log_file = path.dirname(__file__) + "\\follower.log"
 
@@ -13,8 +14,18 @@ def log(message):
     with open(log_file, "a+") as f:
         f.write(message + "\n")
 
+def currentTime():
+    now = datetime.now()
+    return now.strftime("%d/%m/%Y %H:%M:%S") + " - "
+
 cl = Client()
-cl.login(username, password)
+
+try:
+    cl.login(username, password)
+except: 
+    print(currentTime() + "Failed to login, please restart application")
+    time.sleep(10)
+    quit()
 
 user_id = cl.user_id_from_username(user_to_observe)
 
@@ -26,10 +37,10 @@ def dictToList(dict):
 
     return return_list
 
-print("Fetching Followers and Following")
+print(currentTime() + "Fetching Followers and Following")
 global_followers = dictToList(cl.user_followers(user_id).items())
 global_following = dictToList(cl.user_following(user_id).items())
-print("Fetched!")
+print(currentTime() + "Fetched!")
 
 
 # Check if the lists have differences
@@ -42,27 +53,27 @@ def checkLists(new_list, old_list):
         if user in old_list:
             continue
         if user not in old_list:
-            log ("new follower - " + user)
+            log (currentTime() + "new follower - " + user)
             new_follower.append(user)
     
     for user in old_list:
         if user in new_list:
             continue
         if user not in new_list:
-            log("new unfollow - " + user)
+            log(currentTime() + "new unfollow - " + user)
             new_unfollow.append(user)
     
     return new_follower, new_unfollow
 
 
 while True:
-    print("Checking in 600 seconds")
-    time.sleep(600)
-    print("Checking now")
+    print(currentTime() + "Checking in 60 seconds")
+    time.sleep(60)
+    print(currentTime() + "Checking now")
     temp_follower = dictToList(cl.user_followers(user_id, False).items())
     temp_following = dictToList(cl.user_following(user_id, False).items())
-    print("Len Follower " + str(len(temp_follower)), "Len Following " + str(len(temp_following)), sep=" ")
-    print("Fetched new Lists")
+    print(currentTime() + "Len Follower " + str(len(temp_follower)), "Len Following " + str(len(temp_following)), sep=" ")
+    print(currentTime() + "Fetched new Lists")
     result_follower = checkLists(temp_follower, global_followers)
     result_following = checkLists(temp_following, global_following)
 
@@ -70,16 +81,5 @@ while True:
     global_following = temp_following
 
 
-    print("New followers: " + str(result_follower[0]), "New unfollower: " + str(result_follower[1]), sep=" ")
-    print("New following: " + str(result_following[0]), " New unfollowed: " + str(result_following[1]), sep=" ")
-
-
-
-    
-
-
-
-
-
-
-
+    print(currentTime() + "New followers: " + str(result_follower[0]), "New unfollower: " + str(result_follower[1]), sep=" ")
+    print(currentTime() + "New following: " + str(result_following[0]), " New unfollowed: " + str(result_following[1]), sep=" ")
